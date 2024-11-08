5b4a99cac7327071e5bc3c71a0be8870f1c5d496
marianella.polo-p <marianella.polo-p@labinfo.is.escuelaing.edu.co>
2024-11-08 09:12:55 -0500

    Added prepareElemets and main

--- Clustering/src/presentation/Clustering_GUI.java
+++ Clustering/src/presentation/Clustering_GUI.java
@@ -1,13 +1,55 @@
 package Clustering.src.presentation;
 
 import javax.swing.*;
+import src.*
 
 public class Clustering_GUI extends JFrame {
 
-    private void createMainWindow(){
+    private static final Dimension PREFERED_DIMENSION = new Dimension(1000,200);
 
+    private final Clustering game;
+
+    /*Barra superior*/
+    private JMenuBar menuBar;
+    private JMenu menu;
+    private JMenu config;
+    private JMenuItem createTileMenuItem;
+    private JMenuItem deleteTileMenuItem;
+    private JMenuItem exitGameMenuItem;
+
+    /*Scores*/
+    private JLabel textScore;
+    private JLabel score;
+    private JLabel textMoves;
+    private JLabel moves;
+
+    public Clustering_GUI(){
+        this.game = new Clustering();
+        this.prepareElements;
+        this.prepareActions;
     }
+
     public static void main(String[] args){
-
+        Clustering_GUI gui = new Clustering_GUI();
+        gui.setVisible(true);
     }
+
+    private void prepareElements(){
+        setTitle("Clustering game");
+        menuBar = new JMenuBar();
+        menu = new JMenu("Menu");
+        menuBar.add(menu);
+        createTileMenuItem = new JMenuItem("Create Tile");
+        menu.add(createTileMenuItem);
+        deleteTileMenuItem = new JMenuItem("Delete Tile");
+        menu.add(deleteTileMenuItem);
+        exitGameMenuItem = new  JMenuItem("Exit");
+        menu.add(exitGameMenuItem);
+        frame.setJMenuBar(menuBar);
+        frame.setSize(350,250);
+        frame.setVisible(true);
+    }
+
+
+
 }
